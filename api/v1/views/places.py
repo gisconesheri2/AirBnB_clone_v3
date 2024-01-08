6dae909c1fd0dad98ca904d321ea1470eb02b127
gisconesheri2 <pgikonyo2022@gmail.com>
2024-01-08 16:06:51 +0300

    places search

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -92,3 +92,83 @@ def update_place(place_id):
         setattr(place, key, value)
     place.save()
     return (jsonify(place.to_dict()), 200)
+
+
+@app_views.route('/places_search', methods=['POST'])
+def search_place():
+    """search Places with a query parameters in a json string"""
+    search_dict = request.get_json(silent=True)
+    if not search_dict:
+        abort(400, 'Not a JSON')
+
+    states_terms = search_dict.get('states', None)
+    cities_terms = search_dict.get('cities', None)
+    amenities_terms = search_dict.get('amenities', None)
+    if (len(search_dict) == 0):
+        all_places = storage.all('Place')
+        all_places_list = []
+        for place in all_places.values():
+            all_places_list.append(place.to_dict())
+        return (jsonify(all_places_list))
+
+    if ((states_terms is None) and
+            (cities_terms is None) and
+            (amenities_terms is None)):
+        # if all search keys are empty return all Places
+        all_places = storage.all('Place')
+        all_places_list = []
+        for place in all_places.values():
+            all_places_list.append(place.to_dict())
+        return (jsonify(all_places_list))
+
+    all_places_ids = []
+    all_places_list = []
+    if (states_terms is not None):
+        for state_id in states_terms:
+            state_obj = storage.get('State', state_id)
+            state_cities = state_obj.cities
+            for city in state_cities:
+                places = city.places
+                for place in places:
+                    if place.id not in all_places_ids:
+                        all_places_ids.append(place.id)
+                        all_places_list.append(place.to_dict())
+
+    if (cities_terms is not None):
+        for city_id in cities_terms:
+            city_obj = storage.get('City', city_id)
+            city_places = city_obj.places
+            for place in city_places:
+                if place not in all_places_ids:
+                    all_places_ids.append(place.id)
+                    all_places_list.append(place.to_dict())
+
+    if ((amenities_terms is not None) and
+            (states_terms is None) and
+            (cities_terms is None)):
+        all_places = storage.all('Place')
+        for place in all_places.values():
+            all_places_ids.append(place.id)
+            all_places_list.append(place.to_dict())
+
+    if (amenities_terms is not None):
+        all_places_list = []
+        place_amenity_ids = []
+        found = False
+        for place_id in all_places_ids:
+            place_obj = storage.get('Place', place_id)
+            place_amenities = place_obj.amenities
+            for place_amenity in place_amenities:
+                place_amenity_ids.append(place_amenity.id)
+            for am_id in amenities_terms:
+                if am_id in place_amenity_ids:
+                    found = True
+                else:
+                    found = False
+                    break
+            if found is True:
+                place_obj_dict = place_obj.to_dict()
+                place_obj_dict.pop('amenities')
+                all_places_list.append(place_obj_dict)
+
+    return (jsonify(all_places_list))
